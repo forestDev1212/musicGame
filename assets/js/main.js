6309a23d4dd6ec1e1f392a6d37125d26648b4c63
forest <alexeidorman@gmail.com>
2024-03-20 18:45:32 +0900

    remove all console.log()

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 var musicArry = [];
 var live = 3;
-var score = 0
+var score = 0;
 $("#lives_remaining").text(live);
 var cacheLetter = 0;
 const treble = ["R", "S", "T", "U", "V", "W", "X"];
@@ -49,7 +49,6 @@ function moveLetter(letterObject) {
   newNote.addEventListener("animationend", function () {
     musicArry.splice(musicArry.indexOf(letterObject), 1);
     newNote.remove();
-    console.log(true);
     live--;
     $("#lives_remaining").text(live);
   });
@@ -57,10 +56,7 @@ function moveLetter(letterObject) {
 
 function generateRandomNumber(clefArray, level = 1) {
   // Generate random length between 2 and maxLength
-  const maxLength = clefArray.length + 1;
-  console.log(level);
   const length = Math.floor(Math.random() * level) + 2;
-  console.log(length);
   // Generate first number between 1 and 3
   let randomNumber = clefArray[Math.floor(Math.random() * clefArray.length)];
 
@@ -76,17 +72,14 @@ function generateRandomNumber(clefArray, level = 1) {
 function resetNotes() {}
 
 function checkLetter(letter) {
-  console.log(letter);
   const letters = document.querySelectorAll(".letter");
   if (cacheLetter !== 0) {
-    console.log(cacheLetter);
     const remainder = sevenTone[letter][0] % 10;
     var included = false;
     var matched = false;
     musicArry.forEach((item, index) => {
       if (sevenTone[letter].indexOf(item.value) !== -1 && matched === false) {
         //The New Button is C and Note is C
-        console.log("you are right.");
         musicArry.splice(musicArry.indexOf(item), 1);
         for (let i = 0; i < letters.length; i++) {
           const el = letters[i];
@@ -95,34 +88,28 @@ function checkLetter(letter) {
             cacheLetter = 0;
             $("#symbol_container > span").each(function (index) {
               // Change the content of the span tag
-              console.log(index);
               $(this).text(musicArry[index].string);
             });
             break; // Exit the loop after removing the first matching letter
           }
         }
-        addScore()
+        addScore();
         return;
       } else {
         const remainder = sevenTone[letter][0] % 10;
         const totalNote = cacheLetter * 10 + remainder;
-        console.log(totalNote);
-        console.log(item.value);
         if (totalNote === item.value) {
-          console.log("you are right.");
           musicArry.splice(musicArry.indexOf(item), 1);
           matched = true;
           for (let i = 0; i < letters.length; i++) {
             const el = letters[i];
-            console.log(el.textContent);
-            console.log(item.string);
             if (el.textContent === item.string) {
               el.remove();
               cacheLetter = 0;
               break; // Exit the loop after removing the first matching letter
             }
           }
-          addScore()
+          addScore();
           return;
         } else if (
           totalNote.toString().length < item.value.toString().length &&
@@ -130,8 +117,6 @@ function checkLetter(letter) {
             item.value.toString().substring(0, totalNote.toString().length)
         ) {
           cacheLetter = totalNote;
-          console.log(item.value);
-          console.log(totalNote);
           included = true;
           for (let j = 0; j < letters.length; j++) {
             const el = letters[j];
@@ -231,7 +216,6 @@ function checkLetter(letter) {
                   }
                 }
               }
-              console.log(randomLetter);
               $(el).empty();
               $(el).html(randomLetter);
             }
@@ -247,7 +231,6 @@ function checkLetter(letter) {
             cacheLetter = 0;
             $("#symbol_container > span").each(function (index) {
               // Change the content of the span tag
-              console.log(index);
               $(this).text(musicArry[index].string);
             });
             if (live === 0) {
@@ -262,19 +245,17 @@ function checkLetter(letter) {
     var matched = false;
     var included = false;
     musicArry.forEach((item, index) => {
-      console.log(sevenTone[letter], item.value);
       if (sevenTone[letter].indexOf(item.value) !== -1 && matched === false) {
-        console.log("you are right.");
         matched = true;
         musicArry.splice(musicArry.indexOf(item), 1);
         for (let i = 0; i < letters.length; i++) {
           const el = letters[i];
           if (el.textContent === item.string) {
             el.remove();
+            addScore();
             return; // Exit the loop after removing the first matching letter
           }
         }
-        addScore()
         return;
       } else if (
         sevenTone[letter].indexOf(
@@ -283,9 +264,6 @@ function checkLetter(letter) {
         included === false
       ) {
         cacheLetter = parseInt(item.value.toString().substring(0, 2));
-        console.log(cacheLetter);
-        console.log(letter);
-        console.log(item);
         included = true;
         for (let i = 0; i < letters.length; i++) {
           const el = letters[i];
@@ -371,17 +349,11 @@ function checkLetter(letter) {
                 }
               }
             }
-            console.log(randomLetter);
             $(el).empty();
             $(el).html(randomLetter);
           }
         }
       } else {
-        console.log(letter);
-        console.log("You are Wrong");
-        console.log(included);
-        console.log(matched);
-        console.log(index);
         if (
           index === musicArry.length - 1 &&
           included === false &&
@@ -393,7 +365,6 @@ function checkLetter(letter) {
             $("#lives_remaining").text(live);
             clearInterval(interval);
           }
-          console.log(live);
           return;
         }
       }
@@ -407,7 +378,6 @@ function checkLetter(letter) {
 }
 function gamerOver() {}
 function generateMusicNote(tones = [1, 2, 3], level = 1) {
-  console.log(tones, level);
   const randomNum = generateRandomNumber(tones, level);
   // const randomNum = generateRandomNumber([1, 2, 3]);
   var randomLetter = "'";
@@ -466,13 +436,11 @@ $(document).on(
   function (mode = 3000, tones = [1, 2, 3]) {
     let index = 0;
     function outputMusicNote() {
-      console.log(index);
       // Check if all words have been displayed
       if (index < 3 && live > 0) {
         // Output the word
         generateMusicNote(tones, 1);
         $("#level").text(1);
-        console.log(musicArry);
         // Increment the index for the next word
         index++;
       } else if (10 > index && index >= 3 && live > 0) {
@@ -484,7 +452,6 @@ $(document).on(
         generateMusicNote(tones, 3);
         index++;
       } else {
-        console.log(live);
         // Stop the interval when all words have been displayed
         clearInterval(interval);
         $("#lives_remaining").text(live);
@@ -494,7 +461,6 @@ $(document).on(
       }
     }
     const interval = setInterval(outputMusicNote, 3000);
-    console.log(true);
   }
 );
 
